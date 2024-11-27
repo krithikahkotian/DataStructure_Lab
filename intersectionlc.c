2a792a50d1153a42ebe356db32425d6136e62768
krithikahkotian <krithikah.cs23@bmsce.ac.in>
2024-11-27 10:34:06 +0530

    Create intersectionlc.c

---
+++ intersectionlc.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if(headA==NULL||headB==NULL)
+       return NULL;
+    struct ListNode *ptr1=headA;
+    struct ListNode *ptr2=headB;
+    while(ptr1!=ptr2)
+    {
+        ptr1=(ptr1==NULL)?headB:ptr1->next;
+        ptr2=(ptr2==NULL)?headA:ptr2->next;
+    }
+    return ptr1;
+}
